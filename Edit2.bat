134bf2d4d18791fadb731d974ef624679042cf9c
tayles (Home) <git@tayles.co.uk>
2010-03-27 02:45:05 +0000

    Updated README

--- Edit2.bat
+++
@@ -1,6 +0,0 @@
-
-start git submodule add git://github.com/kerkness/kohana-table.git modules/table
-start git submodule add git://github.com/jonnyjon/Task-Queue.git modules/task-queue
-start git submodule add git://github.com/transphorm/twitter.git modules/twitter
-start git submodule add git://github.com/kohana/codebench.git modules/codebench
-start git submodule add git://github.com/kohana/userguide.git modules/userguide
